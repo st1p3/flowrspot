42a1dab23cbebc7336ef74822172e79e12f03823
st1p3 <stipan07@gmail.com>
2023-05-10 08:46:16 +0200

    ACF plugin added + archive styling

--- wp-content/themes/flowrspot-child/template-parts/archive/content-archive.php
+++ wp-content/themes/flowrspot-child/template-parts/archive/content-archive.php
@@ -1,3 +1,11 @@
+<div class="row mb-3">
+    <div class="col-10 mx-auto border bg-transparent">
+        <div class="input-group">
+            <input type="text" class="form-control rounded-0 border-0 shadow-none bg-transparent" placeholder="Looking for something specific?" aria-label="Flower name" aria-describedby="button-addon2">
+            <button class="btn btn-link color-pink rounded-0 bg-white bg-transparent" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
+        </div>
+    </div>
+</div>
 <div class="row">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="col-6 col-md-3 p-1">
